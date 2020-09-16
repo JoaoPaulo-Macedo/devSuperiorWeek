2f7b7933f8fffeb5974ce57d3a5a97c4ec8c4f5e
joao_paulo <jpmacedocardoso2@gmail.com>
2020-09-16 10:13:04 -0300

    Entities, Repositories

--- backend/src/main/java/com/devsuperior/dspesquisa/DspesquisaApplication.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/DspesquisaApplication.java
@@ -2,12 +2,19 @@ package com.devsuperior.dspesquisa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class DspesquisaApplication {
-
+	//In Java, the classes must have the same name as the file.
+	
 	public static void main(String[] args) {
+		//main method is the entry point of your Java application,
+		//just as in many other languages.
+		
 		SpringApplication.run(DspesquisaApplication.class, args);
+		//Just like runApp() from Flutter, this method initialize
+		//a Spring application.
 	}
-
 }
